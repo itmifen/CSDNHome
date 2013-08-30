db4c9252e745c7516494568a191bc8c482b7ba7f
joylee <joylee@10.0.88.33>
2013-08-30 15:24:10 +0800

    瀑布流优化

--- src/com/joylee/csdnhome/myslideActivity.java
+++ src/com/joylee/csdnhome/myslideActivity.java
@@ -19,6 +19,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import com.joylee.common.AsyncImageLoader;
 import com.joylee.entity.newsentity;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.origamilabs.library.views.StaggeredGridView;
 
 import java.util.ArrayList;
@@ -43,63 +49,121 @@ public class myslideActivity  extends Activity {
     StaggeredGridView gridView;
 
     private MyHandler myHandler;
+    
+    DisplayImageOptions options;
+    
+    protected ImageLoader imageLoader;
 
 
     private String urls[] = {
-            "http://t2.baidu.com/it/u=1107959548,2846438166&fm=23&gp=0.jpg",
-            "http://t1.baidu.com/it/u=387538671,2473224000&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=1957344197,1727460690&fm=23&gp=0.jpg",
-            "http://t1.baidu.com/it/u=813492054,1888949070&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=1262100614,561709515&fm=23&gp=0.jpg",
-            "http://t2.baidu.com/it/u=1064891170,3563175148&fm=23&gp=0.jpg",
-            "http://t2.baidu.com/it/u=3534177823,4284169540&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=3600426509,3698677111&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=3515104138,2079115592&fm=23&gp=0.jpg",
-            "http://t1.baidu.com/it/u=387538671,2473224000&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=1957344197,1727460690&fm=23&gp=0.jpg",
-            "http://t1.baidu.com/it/u=813492054,1888949070&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=1262100614,561709515&fm=23&gp=0.jpg",
-            "http://t2.baidu.com/it/u=1064891170,3563175148&fm=23&gp=0.jpg",
-            "http://t2.baidu.com/it/u=3534177823,4284169540&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=3600426509,3698677111&fm=23&gp=0.jpg",
-            "http://t1.baidu.com/it/u=387538671,2473224000&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=1957344197,1727460690&fm=23&gp=0.jpg",
-            "http://t1.baidu.com/it/u=813492054,1888949070&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=1262100614,561709515&fm=23&gp=0.jpg",
-            "http://t2.baidu.com/it/u=1064891170,3563175148&fm=23&gp=0.jpg",
-            "http://t2.baidu.com/it/u=3534177823,4284169540&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=3600426509,3698677111&fm=23&gp=0.jpg",
-            "http://img.hb.aicdn.com/218d392a45b71e09ebcc4254def46d0ed46f64da6e5c-Z9DM2X_fw236",
-            "http://img.hb.aicdn.com/801dd2a0d8d64961be5b9368d86229f74febcb261e12d-mAzY4P_fw236",
-            "http://img.hb.aicdn.com/a8d9cf3554d80fd07575fc14cfc5d8c05a5d25aff417-9rVPAK_fw236",
-            "http://t1.baidu.com/it/u=387538671,2473224000&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=1957344197,1727460690&fm=23&gp=0.jpg",
-            "http://t1.baidu.com/it/u=813492054,1888949070&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=1262100614,561709515&fm=23&gp=0.jpg",
-            "http://t2.baidu.com/it/u=1064891170,3563175148&fm=23&gp=0.jpg",
-            "http://t2.baidu.com/it/u=3534177823,4284169540&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=3600426509,3698677111&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=1957344197,1727460690&fm=23&gp=0.jpg",
-            "http://t1.baidu.com/it/u=813492054,1888949070&fm=23&gp=0.jpg",
-            "http://t3.baidu.com/it/u=1262100614,561709515&fm=23&gp=0.jpg",
-            "http://t2.baidu.com/it/u=1064891170,3563175148&fm=23&gp=0.jpg",
-            "http://t2.baidu.com/it/u=3534177823,4284169540&fm=23&gp=0.jpg",
-            "http://img.hb.aicdn.com/218d392a45b71e09ebcc4254def46d0ed46f64da6e5c-Z9DM2X_fw236",
-            "http://img.hb.aicdn.com/801dd2a0d8d64961be5b9368d86229f74febcb261e12d-mAzY4P_fw236"
+    		 "http://t2.baidu.com/it/u=1107959548,2846438166&fm=23&gp=0.jpg",
+             "http://t1.baidu.com/it/u=387538671,2473224000&fm=23&gp=0.jpg",
+             "http://t3.baidu.com/it/u=1957344197,1727460690&fm=23&gp=0.jpg",
+             "http://t1.baidu.com/it/u=813492054,1888949070&fm=23&gp=0.jpg",
+             "http://img.segbuy.com/images/2013/07/30/10224243409auto.jpg",
+             "http://img.segbuy.com/images/2013/07/30/10224243707auto.jpg",
+             "http://img.segbuy.com/images/2013/07/16/15354651555auto.jpg",
+             "http://t1.baidu.com/it/u=387538671,2473224000&fm=23&gp=0.jpg",
+             "http://t3.baidu.com/it/u=1957344197,1727460690&fm=23&gp=0.jpg",
+             "http://img.segbuy.com/images/2013/07/23/15451156590auto.jpg",
+             "http://img.segbuy.com/images/2013/07/18/14554613996auto.jpg",
+             "http://img.segbuy.com/images/2013/07/18/14554666695auto.jpg",
+             "http://img.segbuy.com/images/2013/07/18/14554676413auto.jpg",
+             "http://img.segbuy.com/images/2013/07/18/14554621547auto.jpg",
+             "http://img.segbuy.com/images/2013/07/18/14554698393auto.jpg",
+             "http://t1.baidu.com/it/u=813492054,1888949070&fm=23&gp=0.jpg",
+             "http://t3.baidu.com/it/u=1262100614,561709515&fm=23&gp=0.jpg",
+             "http://t2.baidu.com/it/u=1064891170,3563175148&fm=23&gp=0.jpg",
+             "http://t2.baidu.com/it/u=3534177823,4284169540&fm=23&gp=0.jpg",
+             "http://img.segbuy.com/images/2013/07/23/15451156590auto.jpg",
+             "http://img.segbuy.com/images/2013/07/18/14554613996auto.jpg",
+             "http://img.segbuy.com/images/2013/07/18/14554666695auto.jpg",
+             "http://img.segbuy.com/images/2013/07/18/14554676413auto.jpg",
+             "http://img.segbuy.com/images/2013/07/18/14554621547auto.jpg",
+             "http://img.segbuy.com/images/2013/07/18/14554698393auto.jpg",
+             "http://t2.baidu.com/it/u=1064891170,3563175148&fm=23&gp=0.jpg",
+             "http://t2.baidu.com/it/u=3534177823,4284169540&fm=23&gp=0.jpg",
+             "http://t3.baidu.com/it/u=3600426509,3698677111&fm=23&gp=0.jpg",
+             "http://img.hb.aicdn.com/218d392a45b71e09ebcc4254def46d0ed46f64da6e5c-Z9DM2X_fw236",
+             "http://img.hb.aicdn.com/801dd2a0d8d64961be5b9368d86229f74febcb261e12d-mAzY4P_fw236",
+             "http://img.hb.aicdn.com/a8d9cf3554d80fd07575fc14cfc5d8c05a5d25aff417-9rVPAK_fw236",
+             "http://t1.baidu.com/it/u=387538671,2473224000&fm=23&gp=0.jpg",
+             "http://t3.baidu.com/it/u=1957344197,1727460690&fm=23&gp=0.jpg",
+             "http://t1.baidu.com/it/u=813492054,1888949070&fm=23&gp=0.jpg",
+             "http://t3.baidu.com/it/u=1262100614,561709515&fm=23&gp=0.jpg",
+             "http://t2.baidu.com/it/u=1064891170,3563175148&fm=23&gp=0.jpg",
+             "http://t2.baidu.com/it/u=3534177823,4284169540&fm=23&gp=0.jpg",
+             "http://img.hb.aicdn.com/15e250c08262561a2b72fcbfabd3782b609c0e7916b53-3eiaGe_fw580",
+             "http://img.hb.aicdn.com/1bc87b69c537e536a0be5ab8d681fa1603cced7726f88-YrnK3W_fw580",
+             "http://img.hb.aicdn.com/95006ce2a3ded7518b08959d295dae3939b7dca5bd7f9-M9tEks_fw580w",
+             "http://img.hb.aicdn.com/f8c6b1ababac8d5e7633cb22fdc5f25e0a143d8bc554-RaTMGs_fw580w",
+             "http://img.hb.aicdn.com/d52f1ca2a7562e124372e16625fc8ecc56a3286017514-XOLLYz_fw580w",
+             "http://img.hb.aicdn.com/c104256cd7f2d2422ba4b84b3503c84889a8e8cb65082-OVYldq_fw580",
+             "http://img.hb.aicdn.com/d221a8d55acff1621ddd84002a2e3f2241f89cb1473b6-yIwzjF_fw580",
+             "http://t3.baidu.com/it/u=3600426509,3698677111&fm=23&gp=0.jpg",
+             "http://t3.baidu.com/it/u=1957344197,1727460690&fm=23&gp=0.jpg",
+             "http://t1.baidu.com/it/u=813492054,1888949070&fm=23&gp=0.jpg",
+             "http://t3.baidu.com/it/u=1262100614,561709515&fm=23&gp=0.jpg",
+             "http://t2.baidu.com/it/u=1064891170,3563175148&fm=23&gp=0.jpg",
+             "http://t2.baidu.com/it/u=3534177823,4284169540&fm=23&gp=0.jpg",
+             "http://img.segbuy.com/images/2013/08/08/09274372045auto.png",
+             "http://img.segbuy.com/images/2013/08/08/09274310222auto.jpg",
+             "http://img.hb.aicdn.com/218d392a45b71e09ebcc4254def46d0ed46f64da6e5c-Z9DM2X_fw236",
+             "http://img.hb.aicdn.com/801dd2a0d8d64961be5b9368d86229f74febcb261e12d-mAzY4P_fw236",
+             "http://img.segbuy.com/images/2013/08/08/09274480785auto.jpg",
+             "http://img.segbuy.com/images/2013/08/07/14380285063auto.jpg",
+             "http://img.segbuy.com/images/2013/07/30/10224243409auto.jpg",
+             "http://img.segbuy.com/images/2013/07/30/10224243707auto.jpg",
+             "http://img.hb.aicdn.com/85fe2406468e1b64e1b81602f6cb60b912828a581833d-UW6olU_fw580",
+             "http://img.segbuy.com/images/2013/07/16/15354651555auto.jpg",
+             "http://img.hb.aicdn.com/cbcb4f1015040f34457b51105498c716641b6d278daa6-67T0J7_fw580w",
+             "http://img.hb.aicdn.com/15e250c08262561a2b72fcbfabd3782b609c0e7916b53-3eiaGe_fw580",
+             "http://img.hb.aicdn.com/1bc87b69c537e536a0be5ab8d681fa1603cced7726f88-YrnK3W_fw580",
+             "http://img.hb.aicdn.com/95006ce2a3ded7518b08959d295dae3939b7dca5bd7f9-M9tEks_fw580w",
+             "http://img.hb.aicdn.com/f8c6b1ababac8d5e7633cb22fdc5f25e0a143d8bc554-RaTMGs_fw580w",
+             "http://img.hb.aicdn.com/d52f1ca2a7562e124372e16625fc8ecc56a3286017514-XOLLYz_fw580w",
+             "http://img.hb.aicdn.com/c104256cd7f2d2422ba4b84b3503c84889a8e8cb65082-OVYldq_fw580",
+             "http://img.hb.aicdn.com/d221a8d55acff1621ddd84002a2e3f2241f89cb1473b6-yIwzjF_fw580",
+             "http://img.hb.aicdn.com/797c40faeb1e0b201df87cc2ae9692b734551f2912e00a-EBBE4w_fw580w"
     };
-    /**
-     * This will not work so great since the heights of the imageViews
-     * are calculated on the iamgeLoader callback ruining the offsets. To fix this try to get
-     * the (intrinsic) image width and height and set the views height manually. I will
-     * look into a fix once I find extra time.
-     */
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myslide);
+        
+        options = new DisplayImageOptions.Builder()
+		//.showImageOnLoading(R.drawable.ic_stub)
+		//.showImageForEmptyUri(R.drawable.ic_empty)
+		//.showImageOnFail(R.drawable.ic_error)
+		.cacheInMemory(true)
+		.cacheOnDisc(true)
+		.bitmapConfig(Bitmap.Config.RGB_565)
+		.imageScaleType(ImageScaleType.IN_SAMPLE_INT)
+		.build();
+        
+        imageLoader = ImageLoader.getInstance();
+//        // 2. 使用默认配置  
+//           ImageLoaderConfiguration configuration = ImageLoaderConfiguration.createDefault(this);  
+//           // 3. 初始化ImageLoader  
+//           imageLoader.init(configuration); 
+           
+           
+           
+           ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
+			.threadPriority(Thread.NORM_PRIORITY - 2)
+			.denyCacheImageMultipleSizesInMemory()
+			.discCacheFileNameGenerator(new Md5FileNameGenerator())
+			.tasksProcessingOrder(QueueProcessingType.LIFO)
+			.writeDebugLogs() // Remove for release app
+			.build();
+	// Initialize ImageLoader with configuration.
+           imageLoader.init(config);
 
         asyncImageLoader = new AsyncImageLoader(new Handler());
         gridView = (StaggeredGridView) this.findViewById(R.id.staggeredGridView1);
+         gridView.setSelectionToTop();
 
         myHandler = new MyHandler();
         MyThread m = new MyThread();
@@ -114,7 +178,9 @@ public class myslideActivity  extends Activity {
         public boolean setViewValue(View view, Object data, String textRepresentation) {
             if ((view instanceof ImageView) & (data instanceof String)) {
                 ImageView iv = (ImageView) view;
-                loadImage(data.toString(), iv);
+                
+                imageLoader.displayImage(data.toString(), iv, options);
+                //loadImage(data.toString(), iv);
                 return true;
             }
             return false;
@@ -169,7 +235,7 @@ public class myslideActivity  extends Activity {
             Log.d("MyHandler", "handleMessage......");
             super.handleMessage(msg);
 
-            for (int i = 0; i < 20; i++) {
+            for (int i = 0; i < urls.length; i++) {
                 Map<String, String> map = new HashMap<String, String>();
                 map.put("url", urls[i]);
                 list.add(map);
